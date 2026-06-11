953a63ad4b8fed45625bfd698d6304c38d6cc0d0
adarshvirsingh1-creator <adarshvirsingh1@gmail.com>
2026-06-11 14:07:06 -0400

    Added Parent Support module with resources and support request form

---
+++ Controllers/ParentSupportController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BabySphere.Controllers
+{
+    public class ParentSupportController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
